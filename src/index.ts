875aca6d5d93b07b24b77052178413a9a0071128
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-08-30 11:41:03 -0500

    feat!: rename getFileList to getFile

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,27 @@ async function loadFileList(path: string) {
   return cache[path];
 }
 
+export async function getList() {
+  const files = await loadFileList(PATH_TO_ZIPPEED);
+  return files.map((d) => d.name);
+}
+
+export async function getFile(name: string) {
+  const fileList = await loadFileList(PATH_TO_ZIPPEED);
+  const file = fileList.find((file) => file.name === name);
+
+  if (!file) {
+    throw new Error(`There is not a zip file for ${name}`);
+  }
+
+  return file;
+}
+
+export async function getData(name: string) {
+  const file = await getFile(name);
+  return file.arrayBuffer();
+}
+
 export function getZipped() {
   return loadFileList(PATH_TO_ZIPPEED);
 }
