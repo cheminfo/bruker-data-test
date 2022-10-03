01fb6fb0651d8b326b8024f8329c62187969fc6a
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2022-10-03 09:44:12 -0500

    feat!: use FileCollection
    * fix: update filelist-utils
    
    * chore: fix names
    
    * chore: update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { join } from 'path';
 
-import {
-  FileCollection,
-  fileCollectionFromPath,
-  FileCollectionItem,
-} from 'filelist-utils';
+import { fileCollectionFromPath, FileCollection } from 'filelist-utils';
 
 const root = join(__dirname, '../data');
 const PATH_TO_ZIPPED = join(root, 'zipped');
@@ -12,7 +8,7 @@ const PATH_TO_COFFEE = join(root, 'flat/coffee');
 
 const cache: Record<string, FileCollection> = {};
 
-async function loadFileList(path: string) {
+async function loadFileCollection(path: string) {
   if (cache[path]) {
     return cache[path];
   }
@@ -26,13 +22,13 @@ async function loadFileList(path: string) {
 }
 
 export async function getList() {
-  const files = (await loadFileList(PATH_TO_ZIPPED)).items;
-  return files.map((d: FileCollectionItem) => d.name);
+  const fileCollection = await loadFileCollection(PATH_TO_ZIPPED);
+  return fileCollection.files.map((d) => d.name);
 }
 
 export async function getFile(name: string) {
-  const fileList = (await loadFileList(PATH_TO_ZIPPED)).items;
-  const file = fileList.find((file) => file.name === name);
+  const fileCollection = await loadFileCollection(PATH_TO_ZIPPED);
+  const file = fileCollection.files.find((file) => file.name === name);
 
   if (!file) {
     throw new Error(`There is not a zip file for ${name}`);
@@ -47,9 +43,9 @@ export async function getData(name: string) {
 }
 
 export function getZipped() {
-  return loadFileList(PATH_TO_ZIPPED);
+  return loadFileCollection(PATH_TO_ZIPPED);
 }
 
 export function getCoffee() {
-  return loadFileList(PATH_TO_COFFEE);
+  return loadFileCollection(PATH_TO_COFFEE);
 }
